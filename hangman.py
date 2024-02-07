edb0d45a3f71faea5b839884b40dbee49681e1bd
DarrenCNLee <101904359+DarrenCNLee@users.noreply.github.com>
2024-02-06 20:46:17 -0800

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python3
+
+import random
+
+words = []
+debug = False
+
+def main():
+    wordsFile = open("words.txt", "r")
+    
+    while True:
+        line = wordsFile.readline()
+        
+        if not line:
+            break
+        
+        words.append(line)
+        
+    wordsFile.close()
+    
+    
+    solutionWord = words[random.randint(0, len(words) - 1)].rstrip()
+    solution = list(solutionWord)
+    solLetters = set(solution)
+    
+    wrongLetters = set()
+    wrongGuessesLeft = 6
+    
+    guessed = ["_" for _ in range(len(solution))]
+    
+    alphabet = set("abcdefghijklmnopqrstuvwxyz")
+    
+    if debug:
+        print(solution)
+    
+    while wrongGuessesLeft > 0 and guessed != solution:    
+        letter = input("Guess a letter: ")
+        
+        if len(letter) != 1 or letter not in alphabet:
+            print("Please enter one letter.\n")
+            print()
+            continue
+        
+        if letter in solLetters:
+            for i in range(len(guessed)):
+                if solution[i] == letter:
+                    guessed[i] = letter
+                    
+                print(guessed[i] + " ", end = "")
+                
+            print()
+            
+        else:
+            if letter not in wrongLetters:
+                wrongGuessesLeft -= 1
+                wrongLetters.add(letter)
+            print("\"" + letter + "\" was not in the word.")
+            print("Wrong guesses so far:", wrongLetters)
+            print("Wrong guesses left: " + str(wrongGuessesLeft))
+        
+        print("\n")
+            
+            
+    if wrongGuessesLeft > 0:
+        print("Congratulations, you guessed the word correctly!")
+    else:
+        print("Sorry, you took too many turns.")
+        print("The word was: " + solutionWord)
+            
+if __name__ == "__main__":
+    main()
